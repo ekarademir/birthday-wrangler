2f1b8ade9f9fcd6fd651afa9f751a9436a52f4a3
Ertugrul Karademir <ekarademir@gmail.com>
2016-10-30 04:17:11 +0000

    Fetching function completed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# birthday-wrangler
+# birthday-wrangler.py
 A data wrangler using python to crawl, sift, and store birthday dates of well known people from date pages on Wikipedia.
 
-This is a project that I've done previously with PHP. Now I want to develop the same project with Python.
+This was a project that I've done previously with PHP. Here, I develop the same project with Python. This is an exercise for me to learn about http interfacing with requests and text parsing.
--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import requests, re
+
+# Fetch the contents of the date page
 r = requests.get('https://en.wikipedia.org/wiki/March_6')
 
-
-
-#births_section_pattern = re.compile('DOC', re.MULTILINE)
-
-#births_section = re.search('(Births)','>Births</span><span class') #r.content
-births_pattern = '<span class="mw-headline" id="Births">.*<span class="mw-headline" id="Deaths">'
+# Seperate the birth section [everything between span classes of Births and Deaths]
+# Before Reg Exp search, flatten the whole string otherwise
+births_pattern = '<span class="mw-headline" id="Births">(.\n)*<span class="mw-headline" id="Deaths">'
 births_section = re.search(births_pattern, r.content.replace('\n','\00'))
 
 births_section = births_section.group()
-#births_section = births_section.replace('<li>', '\00')
 births_section = births_section.replace('</li>', '\n')
 
 items_pattern = r'<li>.*'
 births_items = re.findall(items_pattern, births_section)
+
+
 
 #print isinstance(births_section.group(), basestring)
 print len(births_items)
